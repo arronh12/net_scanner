aadcbc25436a696becc995aac1df2a465d6c2edc
arronh12 <arronhoare@gmail.com>
2020-05-23 20:00:42 -0400

    Added function to take in an ip address and use it with broadcast MAC address to send out an ARP request to determine MAC of # requested IP.

--- net_scanner.py
+++ net_scanner.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 
-import scapy.all as scapy
+import scapy.all as scapy #
 
 
+# function to take in an ip address and use it with broadcast MAC address to send out an ARP request to determine MAC of
+# requested IP.
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_req_bcast = broadcast/arp_request
+    answered, unanswered = scapy.srp(arp_req_bcast, timeout=2)
+    print(answered.summary())
+    print("\n")
+    print(unanswered.summary())
 
 
 # function calls
-scan("10.0.2.1")
+scan("10.0.2.1/24")
