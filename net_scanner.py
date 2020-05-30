eb02d8451546c478e6db4115bec0960630ec0cb1
arronh12 <arronhoare@gmail.com>
2020-05-29 22:14:41 -0400

    result printing code brought into separate function that takes input from scan function. Scan results put into list of dictionaries to be passed into print result function for better user output.

--- net_scanner.py
+++ net_scanner.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import scapy.all as scapy #
+import scapy.all as scapy
 
 
 # function to take in an ip address and use it with broadcast MAC address to send out an ARP request to determine MAC of
@@ -11,12 +11,24 @@ def scan(ip):
     arp_req_bcast = broadcast/arp_request
     answered = scapy.srp(arp_req_bcast, timeout=2, verbose=False)[0]
 
-    print("\n---------------------------------------------------------")
-    print("IP Address\t\t\tMAC Address\n---------------------------------------------------------")
+    clients_list = []
 
     for e in answered:
-        print(e[1].psrc + "\t\t\t" + e[1].hwsrc)
+        client_dict = {"ip": e[1].psrc, "mac": e[1].hwsrc}
+        clients_list.append(client_dict)
+
+    return clients_list
+
+
+# function to take scan result to format and print for user.
+def print_result(results_list):
+    print("\n---------------------------------------------------------")
+    print("IP Address\t\t\tMAC Address\n---------------------------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t\t" + client["mac"])
 
 
 # function calls
-scan("10.0.2.1/24")
+scan_result = scan("10.0.2.1/24")
+print_result(scan_result)
+
